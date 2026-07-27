4b70d161260f8fa8843b190f5a3880912e9e6f51
riceballeater <64981373+riceballeater@users.noreply.github.com>
2026-07-27 19:56:30 -0400

    created layout

--- popup/tamagotchi.js
+++ popup/tamagotchi.js
@@ -1,1 +1,6 @@
-console.log("hello");
+let tamagotchi = document.getElementById("tamagotchi");
+
+tamagotchi.classList.add("hungry");
+tamagotchi.addEventListener("click", e => {
+    tamagotchi.classList.remove("hungry");
+});
